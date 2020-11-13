e7d2db093e8dffc566d544082e7b3680ae763517
Rick Toews <rick.toews@gmail.com>
2020-11-12 22:30:22 -0800

    Make selected letter transition out shortly after displayed.

--- src/components/LargeLetter.js
+++ src/components/LargeLetter.js
@@ -1,19 +1,43 @@
 import React, { useEffect, useState } from 'react';
 
+/*
+Start: display: flex; opacity: 1, transition-duration: 1s
+
+End: display: none; opacity: 1
+*/
+
 function LargeLetter(props) {
 	const classes = ['letter-container'];
 
+	var letterEl;
+
+	useEffect(() => {
+		if (props.letter) {
+			letterEl = document.querySelector('.letter-container');
+			letterEl.classList.add('start-trans');
+			letterEl.addEventListener('transitionend', () => {
+				props.setLargeLetter('');
+				letterEl.classList.remove('start-trans');
+			});
+		}
+	}, [props.letter]);
+	
 	if (props.letter && props.correct) {
 		classes.push('correct');
 	}
 
-//	console.log('LargeLatter', props, classes);
+	
 
 	return (
 	<div style={{ display: props.letter ? 'flex' : 'none' }} className="large-letter">
-		<div className={classes.join(' ')}>
+		{ !props.letter ? (<div style={{ backgroundColor: 'purple' }} className={classes.join(' ')}>
 		{props.letter}
-		</div>
+		</div>)
+
+		: (<div className={classes.join(' ')}>
+		{props.letter}
+		</div>)
+	        }
 	</div>
 	);
 }
